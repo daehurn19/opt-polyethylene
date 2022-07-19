e9af5ebb043de5f45af168a05412a2776e75afb4
daehurn19 <61763092+daehurn19@users.noreply.github.com>
2022-07-19 10:09:36 +0100

    Add files via upload

--- Rand_Perturbation.py
+++ Rand_Perturbation.py
@@ -1,61 +1,88 @@
 import numpy as np
 
 
-test = [np.array([[0,0,0],[0,0.45,0.8],[0,0.45,-0.8]]),np.array([[0,0,0],[0,0.45,0.8],[0,0.45,-0.8]])]
-test2 = [['C','H','H'],['C,H,H']]
 def Randomise_PE(AP_all_chains, AMP):
-    # Takes a normalised Polyethylene and applies a random perturbation to all atoms in the chain.
-    # Ensure the chain does not self-overlap, save each new position in a list, and re-perturb if new member is within
-    #  0.75A same spatial coordinates of C or 0.5A same spatial coordinates of H
+    # Takes all the normalised Polyethylene from Build_PE and applies a random perturbation to all atoms.
+    # Ensure the chain does not self-overlap... model each atom as hard spheres, and do not let their spheres overlap..
+    # If they overlap, reperturb, or move slightly until its ok.
 
-    New_AP_all_chains = [] # Perturbed version of N chains
-
+    New_AP_all_chains = [] # Perturbed version of N polymer chains
 
     for i in AP_all_chains: # sequentially move through list of N polymer fragment positions
+        New_AP = []  # separate A_P perturbations for i polymer.
 
-        New_AP = []  # seperate A_P perturbations for i polymer.
-
-        for j in i: #Sequentially move through list of A_P for i polymer.
-            c = len(New_AP)  # Checks for C or H (1, 4, 7 .. = C) (2,3, 5,6 .. = H)
-            print (str(j) + " current j")
+        for j in i: #Sequentially move through list of atoms for i polymer.
+            # Checks for C or H (1, 4, 7 .. = C) (2,3, 5,6 .. = H)
             # Perturb j by AMP
-            flag = True #True, until perturbed_j successfully appended to new_AP
+            flag = True  #True, until perturbed_j successfully appended to new_AP
             while flag:
                 rand = np.random.uniform(-AMP, AMP, size=(3))
                 perturbed_j = j+rand
-                abs_j = np.sqrt(perturbed_j[0]**2 + perturbed_j[1]**2 + perturbed_j[2]**2)
 
                 if len(New_AP) > 0:
 
-                    for k in range(c):
-                        print (str(k) + " current k")
-                        if (k+1) % 3 == 1: #If it is a carbon comparison
-                            if abs(abs_j - np.sqrt(New_AP[k][0]**2 + New_AP[k][1]**2 + New_AP[k][2]**2)) >= 0.75:
-                                print ("success - the atom does not overlap with an existing C")
-                            else:
-                                print ("fail - the atom overlaps with an existing C")
-                                break
-                        else: #If it is hydrogen
-                            if abs(abs_j - np.sqrt(New_AP[k][0]**2 + New_AP[k][1]**2 + New_AP[k][2]**2)) >= 0.5:
-                                print("success - the atom does not overlap with an existing H")
-                            else:
-                                print ("fail - the atom overlaps with an existing H")
-                                break
-                        if (k+1) == c:
-                            flag = False
-                            New_AP.append(perturbed_j)
+                    for k in range(len(New_AP)):
+                        #check if new atom (modelled as a point) is not within sphere of radius 0.75A of any atom in New_AP
+                        if (perturbed_j[0] - New_AP[k][0])**2 + (perturbed_j[1] - New_AP[k][1])**2 + (perturbed_j[2] - New_AP[k][2])**2 < 0.75**2:
+                            break
+
+                    else:
+                        flag = Ftalse
+                        New_AP.append(perturbed_j)
                 else:
                     flag = False
                     New_AP.append(perturbed_j)
-                    print ("Success, first C added")
-        print ("Polymer fragment finished.")
+
         New_AP_all_chains.append(New_AP)
 
     return New_AP_all_chains
 
-print (Randomise_PE(test, 1))
+def Randomise_Position(AP_all_chains, AMP):
+    #Functionally similar to Randomise_PE, but compares between each polyethylene fragment
+    #Rather than self-comparison of each atom position with the same fragment atoms
+    New_AP_all_chains = []
+    for i in AP_all_chains:  # sequentially move through list of N polymer fragment positions
+        for j in i:  # Sequentially move through list of atoms for i polymer.
+            # Checks for C or H (1, 4, 7 .. = C) (2,3, 5,6 .. = H)
+            # Perturb j by AMP
+            flag = True  # True, until perturbed_j successfully appended to new_AP
+            redo = True
+            while flag:
+                if redo:
+                    rand = np.random.uniform(-AMP, AMP, size=(3))
+                perturbed_j = j + rand
+
+                redo = False
+                current_chain = []
+
+                if len(New_AP_all_chains) > 0:  # We do not perturb the position of the first chain.
+                    # check through New_Pos (a list of arrays of atom position for each chain)
+                    # for each member of New_Pos, find the highest and lowest values for x,y,z.
+                    # then check that any member of current_chain is not within 1.5A of these values.
+
+                    for k in New_AP_all_chains:
+                        #Currently a list of atom positions in New_AP_all_chains
+                        for l in k:
+                            #Currently each atom position...
+                            #Do a comparison with current atom position
+                            if (perturbed_j[0] - l[0]) ** 2 + (perturbed_j[1] - l[1]) ** 2 + (
+                                    perturbed_j[2] - l[2]) ** 2 < 1.5 ** 2:
+                                redo = True
+                                break
+                        else:
+                            current_chain.append(perturbed_j)
+                            continue
+
+                        break
+                    flag = False
 
 
+                else:
+                    current_chain.append(j) # If nothing in allchain
+                New_AP_all_chains.append(current_chain)
+
+            #   print ("Successfully translated chain position without conflict.")
+
+    return New_AP_all_chains
 
 
-    
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import numpy as np
 from Build_PE import Build_PE
 from Build_randbox import Acceptability
+from Rand_Perturbation import Randomise_PE, Randomise_Position
+import ase, ase.io
 
 def main(N): # Number of chains to create inside box.
     all_chain_L = []
     num_C = []
-    AP_allchains = []
-    AT_allchains = []
+    AP_allchains = [] #Atom_positions of all N chains, sorted by generation history
+    AT_allchains = [] #Atom_types of all N chains, sorted by generation history
     #build N normal polyethylene chains
     for i in range(N):
-        ran_num = np.random.randint(5,20)
-        AP_allchains.append(Build_PE(ran_num)[0])
-        AT_allchains.append(Build_PE(ran_num)[1])
+        ran_num = np.random.randint(5, 20)
+        AP_allchains.append(Build_PE(2)[0])
+        AT_allchains.append(Build_PE(2)[1])
         all_chain_L.append(AP_allchains[-1][-3][0])
         num_C.append(ran_num)
     print(str(N) + " standardised polyethylene generated.")
 
-    # then build random box to contain chains (with PBC active)
+    # then build random box to contain chains
     flag = True
     rand_box_dim = [] #format is a,b,c,alp,bet,gam
-    count = 0
     while flag:
         acceptable = Acceptability(all_chain_L, num_C)
 
@@ -29,12 +30,46 @@ def main(N): # Number of chains to create inside box.
                 rand_box_dim.append(acceptable[i+1])
             print("Acceptable box configuration found.")
 
-    return (rand_box_dim)
-
-
     # then perturb each chain
-    # then place each chain randomly in box
+    AMP = 2
+    New_AP_allchains = Randomise_PE(AP_allchains, AMP)
+    print(New_AP_allchains)
+    print("All polyethylene molecules randomised.")
+
+    # then place each chain randomly in box # account for PBC
+    # Can be done by making sure there is a minimum 1.5A distance from each chain,
+    # and all chains stay within the confines of the box dimensions in b and c direction.
+    """AMP = 3
+    New_AP_allchains = Randomise_Position(New_AP_allchains, AMP)
+    print (New_AP_allchains)
+    print ("Polyethylene randomly placed within box.")
+"""
+
+    #Now, flatten all lists for output.
+    # Use ASE to do this easily.
+    #We have both rand_box_dim (dimensions of random box), and New_AP_allchains (coordiantes of randomised polethylene fragments)
+
+    elements = ''
+    for s in AT_allchains:
+        for t in s:
+            elements += t
+    print (len(elements))
+
+    flat_AP = []
+    print (len(New_AP_allchains))
+    print (New_AP_allchains)
+    for s in New_AP_allchains: #New_ap_allchains = list of np.arrays of 3 elements
+        for t in s:
+            flat_AP.append(t)
+    print (flat_AP)
 
 
-print(main(10))
+    atoms = ase.Atoms(elements, np.array(flat_AP), cell=rand_box_dim, pbc=True)
+    ase.io.write(r'C:\Users\daehu\Desktop\polyethylene\test.cell', atoms, format='castep-cell')
+    return -1
+
+
+
+print(main(3))
+
 
