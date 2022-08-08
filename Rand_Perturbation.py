a26d467fd7536b1b14f0668449c37a20ea38e5c9
daehurn19 <61763092+daehurn19@users.noreply.github.com>
2022-08-08 02:38:43 +0100

    Add files via upload

--- Rand_Perturbation.py
+++ Rand_Perturbation.py
@@ -1,46 +1,13 @@
 import numpy as np
 
 
-def Randomise_PE(AP_all_chains, AMP):
-    # Takes all the normalised Polyethylene from Build_PE and applies a random perturbation to all atoms.
-    # Ensure the chain does not self-overlap... model each atom as hard spheres, and do not let their spheres overlap..
-    # If they overlap, reperturb, or move slightly until its ok.
-
-    New_AP_all_chains = [] # Perturbed version of N polymer chains
-
-    for i in AP_all_chains: # sequentially move through list of N polymer fragment positions
-        New_AP = []  # separate A_P perturbations for i polymer.
-
-        for j in i: #Sequentially move through list of atoms for i polymer.
-            # Checks for C or H (1, 4, 7 .. = C) (2,3, 5,6 .. = H)
-            # Perturb j by AMP
-            flag = True  #True, until perturbed_j successfully appended to new_AP
-            while flag:
-                rand = np.random.uniform(-AMP, AMP, size=(3))
-                perturbed_j = j+rand
-
-                if len(New_AP) > 0:
-
-                    for k in range(len(New_AP)):
-                        #check if new atom (modelled as a point) is not within sphere of radius 0.75A of any atom in New_AP
-                        if (perturbed_j[0] - New_AP[k][0])**2 + (perturbed_j[1] - New_AP[k][1])**2 + (perturbed_j[2] - New_AP[k][2])**2 < 0.75**2:
-                            break
-
-                    else:
-                        flag = False
-                        New_AP.append(perturbed_j)
-                else:
-                    flag = False
-                    New_AP.append(perturbed_j)
-
-        New_AP_all_chains.append(New_AP)
-
-    return New_AP_all_chains
-
-def Randomise_Position(AP_all_chains, a, b, c):
+def Randomise_Position(AP_all_chains, b, c):
     #Functionally similar to Randomise_PE, but compares between each polyethylene fragment
     #Rather than self-comparison of each atom position with the same fragment atoms
     New_AP_all_chains = []
+
+    #All mirrors are: +0, +b, +c, +bc
+    mirror = []
     count = 0
 
     for i in AP_all_chains:  # sequentially move through list of N polymer fragment positions
@@ -63,12 +30,19 @@ def Randomise_Position(AP_all_chains, a, b, c):
                     # check, that current_chain for EACH CHAIN, does not overlap its atoms with any of their atoms.
                     for chain in New_AP_all_chains:
                         for atom in chain:
-                            if (perturbed_j[0] - atom[0])**2 + (perturbed_j[1] - atom[1])**2 + (perturbed_j[2] - atom[2])**2 < 2**2:
-                                current_chain = []
+                            if (perturbed_j[0] - atom[0])**2 + (perturbed_j[1] - atom[1])**2 + (perturbed_j[2] - atom[2])**2 < 1.5**2:
+                                if (perturbed_j[0] - atom[0])**2 + (perturbed_j[1] - (atom[1]+b))**2 + (perturbed_j[2] - atom[2])**2 < 1.5**2:
+                                    if (perturbed_j[0] - atom[0]) ** 2 + (perturbed_j[1] - (atom[1])) ** 2 + (
+                                            perturbed_j[2] - (atom[2]+c)) ** 2 < 1.5 ** 2:
+                                        if (perturbed_j[0] - atom[0]) ** 2 + (perturbed_j[1] - (atom[1] + b)) ** 2 + (
+                                                perturbed_j[2] - (atom[2]+c)) ** 2 < 1.5 ** 2:
 
-                                redo = True
-                                # if overlaps, then break out of for loops before satisfying the below if statement.
-                                break
+
+                                            current_chain = []
+
+                                            redo = True
+                                            # if overlaps, then break out of for loops before satisfying the below if statement.
+                                            break
 
                         else:
                             continue
